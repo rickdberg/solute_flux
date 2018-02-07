f929e2fd6636863384ace83aa166c3a873b6efcd
Rick Berg <rickdberg@gmail.com>
2018-02-07 13:59:00 -0800

    Made variable names consistent with pep8

--- interface_flux.py
+++ interface_flux.py
@@ -12,16 +12,16 @@ Module is designed to be paired with a MySQL database of ocean drilling data.
 See flux_functions.py for detailed descriptions of each function used.
 
 Inputs:
-Leg:            drilling leg/expedition number
-Site:           drilling site number
-Holes:          drilling hole IDs
-Comments:       comments for this location or dataset
-Solute:         solute name in database
-Ds:             diffusion coefficient at reference temperature (m^2 y^-1)
-TempD:          reference temperature of diffusion coefficient (C)
-Precision:      relative standard deviation of concentration measurements
-Ocean:          concentration of conservative solute in the ocean (millimolar)
-Solute_db:      solute name for inserting into database
+leg:            drilling leg/expedition number
+site:           drilling site number
+holes:          drilling hole Ids
+comments:       comments for this location or dataset
+solute:         solute name in database
+ds:             diffusion coefficient at reference temperature (m^2 y^-1)
+temp_d:          reference temperature of diffusion coefficient (C)
+precision:      relative standard deviation of concentration measurements
+ocean:          concentration of conservative solute in the ocean (millimolar)
+solute_db:      solute name for inserting into database
 z:              depth at which flux is calculated (mbsf)
 cycles:         number of monte carlo simulations to run
 line_fit:       "linear" or "exponential" line fit to concentration profile
@@ -59,7 +59,7 @@ stdev_flux_upper:     Lower log-normal value for 1 standard deviation
 skewness_log:         skewness of distribution of log-normal fluxes
 p_value_log:          Kolmogorov-Smirvov p-value of distribution of log-normal fluxes
 runtime_errors:       number of runtime errors resulting from line-fitting procedure
-Date:                 date the site was modeled
+date:                 date the site was modeled
 
 Note: all other ouputs are taken directly from database site information and
 hole information tables.
@@ -81,37 +81,37 @@ from user_parameters import (engine, conctable, portable, metadata_table,
 # User-specified parameters
 
 # Site Information
-Leg = '190'
-Site = '1178'
-Holes = "('A','B') or hole is null"
-Comments = ''
+leg = '190'
+site = '1178'
+holes = "('A','B') or hole is null"
+comments = ''
 
 # Species parameters
-Solute = 'Mg'  # Change X to X_ic if needed, based on availability in database
-Ds = 1.875*10**-2  # m^2 per year
-TempD = 18  # degrees C
-Precision = 0.02
-Ocean = 54
-Solute_db = 'Mg'
+solute = 'Mg'  # Change X to X_ic if needed, based on availability in database
+ds = 1.875*10**-2  # m^2 per year
+temp_d = 18  # degrees C
+precision = 0.02
+ocean = 54
+solute_db = 'Mg'
 
 # Model Parameters
 z = 0
 cycles = 5000
 line_fit = 'exponential'
 dp = 8
-optimized = 'no'  # whether finished optimizing Model Parameters for this site
+optimized = 'yes'  # whether finished optimizing Model Parameters for this site
 
 ###############################################################################
 ###############################################################################
 con = engine.connect()
-Complete = 'yes'
+complete = 'yes'
 runtime_errors = 0
 plt.close('all')
 
 # Load and prepare all input data
 site_metadata = ff.metadata_compiler(engine, metadata_table, site_info,
-                                  hole_info, Leg, Site)
-concunique, temp_gradient, bottom_conc, bottom_temp, bottom_temp_est, pordata, sedtimes, seddepths, sedrate, picks, age_depth_boundaries, advection = ff.load_and_prep(Leg, Site, Holes, Solute, Ocean, engine, conctable, portable, site_metadata)
+                                  hole_info, leg, site)
+concunique, temp_gradient, bottom_conc, bottom_temp, bottom_temp_est, pordata, sedtimes, seddepths, sedrate, picks, age_depth_boundaries, advection = ff.load_and_prep(leg, site, holes, solute, ocean, engine, conctable, portable, site_metadata)
 # concunique = concunique[1:,:]  # If you don't use bottom water concentration
 
 if not sedtimes.size:
@@ -141,31 +141,31 @@ porosity = ff.por_curve(z, por, por_fit)[0]
 tortuosity = 1-np.log(porosity**2)
 
 # Calculate effective diffusion coefficient
-D_in_situ = ff.d_stp(TempD, bottom_temp, Ds)
-Dsed = D_in_situ/tortuosity  # Effective diffusion coefficient
+d_in_situ = ff.d_stp(temp_d, bottom_temp, ds)
+dsed = d_in_situ/tortuosity  # Effective diffusion coefficient
 
 # Calculate pore water burial flux rate
 pwburialflux = ff.pw_burial(seddepths, sedtimes, por_fit, por)
 
 # Calculate solute flux
 flux, burial_flux, gradient = ff.flux_model(conc_fit, concunique, z,
-                                            pwburialflux, porosity, Dsed,
-                                            advection, dp, Site, line_fit)
+                                            pwburialflux, porosity, dsed,
+                                            advection, dp, site, line_fit)
 
 # Plot input data
 plt.ioff()
 figure_1 = ff.flux_plots(concunique, conc_interp_fit_plot, por, por_all,
                          por_fit, bottom_temp, picks, sedtimes, seddepths,
-                         Leg, Site, Solute_db, flux, dp, temp_gradient)
+                         leg, site, solute_db, flux, dp, temp_gradient)
 figure_1.show()
 if optimized == 'yes':
     # Save Figure
     if flux_fig_path:
         savefig(flux_fig_path +
-               "interface_{}_flux_{}_{}.png".format(Solute_db, Leg, Site))
+               "interface_{}_flux_{}_{}.png".format(solute_db, leg, site))
 
     # Monte Carlo Simulation
-    interface_fluxes, interface_fluxes_log, cycles, por_error, mean_flux, median_flux, stdev_flux, skewness, p_value, mean_flux_log, median_flux_log, stdev_flux_log, stdev_flux_lower, stdev_flux_upper, skewness_log, p_value_log, runtime_errors, conc_fits = ff.monte_carlo(cycles, Precision, concunique, bottom_temp_est, dp, por, por_fit, seddepths, sedtimes, TempD, bottom_temp, z, advection, Leg, Site, Solute_db, Ds, por_error, conc_fit, runtime_errors, line_fit)
+    interface_fluxes, interface_fluxes_log, cycles, por_error, mean_flux, median_flux, stdev_flux, skewness, p_value, mean_flux_log, median_flux_log, stdev_flux_log, stdev_flux_lower, stdev_flux_upper, skewness_log, p_value_log, runtime_errors, conc_fits = ff.monte_carlo(cycles, precision, concunique, bottom_temp_est, dp, por, por_fit, seddepths, sedtimes, temp_d, bottom_temp, z, advection, leg, site, solute_db, ds, por_error, conc_fit, runtime_errors, line_fit)
 
     # Plot Monte Carlo Distributions
     mc_figure =ff.monte_carlo_plot(interface_fluxes, median_flux, stdev_flux,
@@ -177,30 +177,30 @@ if optimized == 'yes':
     # Save figure and fluxes from each run
     if mc_fig_path:
         savefig(mc_fig_path +
-            "monte carlo_{}_{}_{}.png".format(Solute_db, Leg, Site))
+            "monte carlo_{}_{}_{}.png".format(solute_db, leg, site))
     if mc_text_path:
         np.savetxt(mc_text_path +
-            "monte carlo_{}_{}_{}.csv".format(Solute_db, Leg, Site),
+            "monte carlo_{}_{}_{}.csv".format(solute_db, leg, site),
             interface_fluxes, delimiter=",")
 
     # Retrieve misc metadata
-    Hole = ''.join(filter(str.isupper, filter(str.isalpha, Holes)))
-    Date = datetime.datetime.now()
+    hole = ''.join(filter(str.isupper, filter(str.isalpha, holes)))
+    date = datetime.datetime.now()
     site_key = con.execute("""select site_key
                     from site_info
                     where leg = '{}' and site = '{}'
-                    ;""".format(Leg, Site))
+                    ;""".format(leg, site))
     site_key = site_key.fetchone()[0]
 
     # Send metadata to database
-    ff.flux_to_sql(con, Solute_db, site_key,Leg,Site,Hole,Solute,flux,
+    ff.flux_to_sql(con, solute_db, site_key,leg,site,hole,solute,flux,
                    burial_flux,gradient,porosity,z,dp,bottom_conc,conc_fit,
-                   r_squared,age_depth_boundaries,sedrate,advection,Precision,Ds,
-                   TempD,bottom_temp,bottom_temp_est,cycles,por_error,mean_flux,
+                   r_squared,age_depth_boundaries,sedrate,advection,precision,ds,
+                   temp_d,bottom_temp,bottom_temp_est,cycles,por_error,mean_flux,
                    median_flux,stdev_flux,skewness,p_value,mean_flux_log,
                    median_flux_log,stdev_flux_log,stdev_flux_lower,
                    stdev_flux_upper,skewness_log,p_value_log,runtime_errors,
-                   Date,Comments,Complete)
+                   date,comments,complete)
     print('Results sent to database')
 else:
     print('Continue optimization: Results NOT sent to database')
