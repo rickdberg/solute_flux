2582f3c9415f47be090d2580a550d58a09048c20
Rick Berg <rickdberg@gmail.com>
2018-02-07 12:57:56 -0800

    Made variable names consistent with PEP8

--- interface_fractionation.py
+++ interface_fractionation.py
@@ -3,7 +3,7 @@
 Created on Thu Jan 26 16:13:57 2017
 Author: Rick Berg, University of Washington School of Oceanography, Seattle WA
 
-Module to calculate sediment-water isotope fractionations based on fluxes of
+Module to calculate sediment-water Mg isotope fractionations based on fluxes of
 each isotope across the sediment-water interface, accounting for diffusion,
 advection, and pore water burial with compaction. Although meant for the
 sediment-water interface, the module is generalized to calculate the flux
@@ -12,21 +12,19 @@ Module is designed to be paired with a MySQL database of ocean drilling data.
 See flux_functions.py for detailed descriptions of each function used.
 
 Inputs:
-Leg:            drilling leg/expedition number
-Site:           drilling site number
-Holes:          drilling hole IDs
-Comments:       comments for this location or dataset
-Complete:       designate 'yes' when model parameters are acceptable
-Solute:         solute name in database
-Ds:             diffusion coefficient at reference temperature (m^2 y^-1)
-TempD:          reference temperature of diffusion coefficient (C)
-Precision:      relative standard deviation of concentration measurements
-Ocean:          concentration of conservative solute in the ocean (mM)
-Solute_db:      solute name for inserting into database
+leg:            drilling leg/expedition number
+site:           drilling site number
+holes:          drilling hole IDs
+solute:         solute name in database
+ds:             diffusion coefficient at reference temperature (m^2 y^-1)
+temp_d:          reference temperature of diffusion coefficient (C)
+precision:      relative standard deviation of concentration measurements
+ocean:          concentration of conservative solute in the ocean (mM)
+solute_db:      solute name for referencing
 z:              depth at which flux is calculated (mbsf)
 cycles:         number of monte carlo simulations to run
 line_fit:       "linear" or "exponential" line fit to concentration profile
-top_boundary:   designate 'seawater' to use "Ocean" value, otherwise uses the
+top_boundary:   designate 'seawater' to use "ocean" value, otherwise uses the
                 shallowest pore water measurement value for the upper boundary
 dp:             concentration datapoints below seafloor used for line fit
 engine:         SQLAlchemy engine
@@ -58,20 +56,18 @@ import flux_functions as ff
 from user_parameters import (engine, conctable, portable, metadata_table,
                              site_info, hole_info, flux_fig_path, mc_fig_path)
 
-# Site Information
-Leg = '344'
-Site = 'U1414'
-Holes = "('A') or hole is null"
-Comments = ''
-Complete = 'yes'
+# site Information
+leg = '344'
+site = 'U1414'
+holes = "('A') or hole is null"
 
 # Species parameters
-Solute = 'Mg'  # Change X to X_ic if needed, based on availability in database
-Ds = 1.875*10**-2  # m^2 per year (Li & Gregory 1971)
-TempD = 18  # degrees C
-Precision = 0.02
-Ocean = 54  # mM
-Solute_db = 'Mg'
+solute = 'Mg'  # Change X to X_ic if needed, based on availability in database
+ds = 1.875*10**-2  # m^2 per year
+temp_d = 18  # degrees C
+precision = 0.02
+ocean = 54  # mM
+solute_db = 'Mg'
 ct_d26 = [-0.82]
 ct_d25 = [-0.41]
 
@@ -90,9 +86,9 @@ plt.close('all')
 
 # Load and prepare all input data
 site_metadata = ff.metadata_compiler(engine, metadata_table, site_info,
-                                  hole_info, Leg, Site)
+                                  hole_info, leg, site)
 
-concunique, temp_gradient, bottom_conc, bottom_temp, bottom_temp_est, pordata, sedtimes, seddepths, sedrate, picks, age_depth_boundaries, advection = ff.load_and_prep(Leg, Site, Holes, Solute, Ocean, engine, conctable, portable, site_metadata)
+concunique, temp_gradient, bottom_conc, bottom_temp, bottom_temp_est, pordata, sedtimes, seddepths, sedrate, picks, age_depth_boundaries, advection = ff.load_and_prep(leg, site, holes, solute, ocean, engine, conctable, portable, site_metadata)
 concunique = pd.DataFrame(concunique)
 concunique.columns = ['sample_depth', 'mg_conc']
 if top_boundary != 'seawater':
@@ -108,13 +104,15 @@ sql = """select sample_depth, d26mg, d25mg, d26mg_2sd
     where leg = '{}'
     and site = '{}'
     and hole in {}
-    and d26mg is not NULL;""".format(Leg, Site, Holes)
+    and d26mg is not NULL;""".format(leg, site, holes)
 isotopedata = pd.read_sql(sql, engine)
 isotopedata = isotopedata.sort_values(by='sample_depth')
 
-# Calculate d25Mg for data from Higgins and Schrag 2010
+# Calculate d25Mg for data from Higgins and Schrag 2010,
+# based on d25Mg = 0.52*d26Mg, cited in Isotope Geochemistry,
+# William White, pp.365
 if isotopedata.d25mg.isnull().all():
-    isotopedata.d25mg = 0.527 * isotopedata.d26mg
+    isotopedata.d25mg = 0.52 * isotopedata.d26mg
 isotopedata = isotopedata[isotopedata.notnull().all(axis=1)]
 isotopedata_26 = ff.averages(isotopedata['sample_depth'],
                                         isotopedata.iloc[:, 1])
@@ -122,6 +120,7 @@ isotopedata_25 = ff.averages(isotopedata['sample_depth'],
                                         isotopedata.iloc[:, 2])
 isotopedata_26_2sd = ff.averages(isotopedata['sample_depth'],
                                         isotopedata.iloc[:, 3])
+
 # Standard deviation of seawater value of 0.025
 isotopedata_26_1sd = (
     0.5 * np.append([0.05],
@@ -178,8 +177,8 @@ porosity = ff.por_curve(z, por, por_fit)[0]
 tortuosity = 1-np.log(porosity**2)
 
 # Calculate effective diffusion coefficient
-D_in_situ = ff.d_stp(TempD, bottom_temp, Ds)
-Dsed = D_in_situ/tortuosity
+d_in_situ = ff.d_stp(temp_d, bottom_temp, ds)
+dsed = d_in_situ/tortuosity
 
 # Calculate pore water burial flux rate
 pwburialflux = ff.pw_burial(seddepths, sedtimes, por_fit, por)
@@ -203,13 +202,13 @@ for n in np.arange(2):
     # Calculate solute flux
     flux, burial_flux, gradient = (
         ff.flux_model(conc_fit, concunique, z, pwburialflux, porosity,
-                      Dsed, advection, dp, Site, line_fit))
+                      dsed, advection, dp, site, line_fit))
 
     # Plot input data
     plt.ioff()
     figure_1 = ff.flux_plots(concunique, conc_interp_fit_plot, por, por_all,
                              por_fit, bottom_temp, picks, sedtimes,
-                             seddepths, Leg, Site, Solute_db, flux, dp,
+                             seddepths, leg, site, solute_db, flux, dp,
                              temp_gradient)
     figure_1.show()
 
@@ -217,7 +216,7 @@ for n in np.arange(2):
     if flux_fig_path:
         savefig(flux_fig_path +
             "interface_{}_fractionation_{}_{}_{}.png"
-            .format(Solute_db, Leg, Site, n))
+            .format(solute_db, leg, site, n))
     fluxes.append(flux)
 
 # Calculate fractionation based on fluxes
@@ -228,26 +227,26 @@ epsilon = (alpha - 1) * 1000
 # Date and time for database
 Date = datetime.datetime.now()
 
-# Retrieve Site key from database
+# Retrieve site key from database
 site_key = con.execute("""select site_key
                 from site_info
                 where leg = '{}' and site = '{}'
-                ;""".format(Leg, Site))
+                ;""".format(leg, site))
 site_key = site_key.fetchone()[0]
 
 # Insert fractionation factor into database
-Hole = ''.join(filter(str.isupper, filter(str.isalpha, Holes)))
+hole = ''.join(filter(str.isupper, filter(str.isalpha, holes)))
 sql= """insert into metadata_{}_flux (site_key,leg,site,hole,solute,
                alpha)
                VALUES ({}, '{}', '{}', '{}', '{}', {})
                ON DUPLICATE KEY UPDATE alpha={}
-               ;""".format(Solute_db, site_key,Leg,Site,Hole,Solute,
+               ;""".format(solute_db, site_key,leg,site,hole,solute,
                            alpha,
                            alpha)
 con.execute(sql)
 
 # Monte Carlo Simulation
-alpha, epsilon, cycles, por_error, alpha_mean, alpha_median, alpha_stdev, test_stat, p_value, runtime_errors, conc_fits, fluxes = ff.monte_carlo_fract(cycles, Precision, precision_iso, concunique, bottom_temp_est, dp, por, por_fit, seddepths, sedtimes, TempD, bottom_temp, z, advection, Leg, Site, Solute_db, Ds, por_error, conc_fit, runtime_errors, isotopedata, mg26_24_ocean, line_fit)
+alpha, epsilon, cycles, por_error, alpha_mean, alpha_median, alpha_stdev, test_stat, p_value, runtime_errors, conc_fits, fluxes = ff.monte_carlo_fract(cycles, precision, precision_iso, concunique, bottom_temp_est, dp, por, por_fit, seddepths, sedtimes, temp_d, bottom_temp, z, advection, leg, site, solute_db, ds, por_error, conc_fit, runtime_errors, isotopedata, mg26_24_ocean, line_fit)
 
 # Insert Monte Carlo simulation output into database
 sql= """insert into metadata_{}_flux (site_key,leg,site,hole,solute,
@@ -255,7 +254,7 @@ sql= """insert into metadata_{}_flux (site_key,leg,site,hole,solute,
                VALUES ({}, '{}', '{}', '{}', '{}', {}, {}, {}, {})
                ON DUPLICATE KEY UPDATE alpha_mean={}, alpha_median={},
                alpha_stdev={}, alpha_p_value={}
-               ;""".format(Solute_db, site_key,Leg,Site,Hole,Solute,
+               ;""".format(solute_db, site_key,leg,site,hole,solute,
                            alpha_mean, alpha_median, alpha_stdev, p_value,
                            alpha_mean, alpha_median, alpha_stdev, p_value)
 con.execute(sql)
@@ -267,7 +266,7 @@ mc_figure.show()
 if mc_fig_path:
     savefig(mc_fig_path +
         "interface_{}_fractionation_distribution_{}_{}.png"
-        .format(Solute_db, Leg, Site))
+        .format(solute_db, leg, site))
 
 print("Mean Epsilon:", round((alpha_mean-1)*1000, 4))
 print("SD Epsilon:", round(alpha_stdev*1000, 4))
